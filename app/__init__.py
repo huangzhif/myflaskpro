f0664dd470fb8ef18a51e59581259b5aa954d30b
root <root@localhost.localdomain>
2019-10-31 16:23:51 +0800

    优化前端架构

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 from flask import Flask
 from werkzeug.utils import import_string
+from flask_bootstrap import Bootstrap
 
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -13,20 +14,34 @@ blueprints = [
     "app.main.test:abc"
 ]
 
+# 扩展
+bootstrap = Bootstrap()
+
 
 def create_app():
     app = Flask(__name__,instance_relative_config=True)
 
     app.config.from_object('config')
     app.config.from_pyfile("config.py")
-    # app.register_blueprint(abc)
+
+    # 注册扩展
     register_logging(app)
+    register_extensions(app)
 
     for bp_name in blueprints:
         bp = import_string(bp_name)
         app.register_blueprint(bp)
 
     return app
+
+
+def register_extensions(app):
+    """
+    第三方扩展
+    :param app:
+    :return:
+    """
+    bootstrap.init_app(app)
 
 
 def register_logging(app):
--- app/main/test.py
+++ app/main/test.py
@@ -1,10 +1,11 @@
-from flask import current_app,Blueprint
+from flask import current_app,Blueprint,render_template
 
 # 蓝图名字不能与模块名（即文件名）相同
 abc = Blueprint('abc',__name__)
 
 
 @abc.route("/")
+@abc.route("/test")
 def test():
     current_app.logger.info("test log.")
-    return "hello world"
+    return render_template("base.html")
