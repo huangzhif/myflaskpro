561b6492aac5fd1ca4fd83dae5ec5ab046516a07
root <root@localhost.localdomain>
2019-11-01 17:10:08 +0800

    优化登录界面与首页

--- app/main/account.py
+++ app/main/account.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, current_app, render_template
+from app.forms.account import LoginForm
 
 bp_login = Blueprint("bp_login", __name__, url_prefix="/account")
 
 
 @bp_login.route("/login")
 def login():
-    return render_template("account/login.html")
+    form = LoginForm()
+    return render_template("account/login.html", form=form)
