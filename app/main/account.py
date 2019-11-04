0022bc8aae39fd7819fb622a15db1b37ff6fd02f
root <root@localhost.localdomain>
2019-11-04 15:53:11 +0800

    优化前端结构，登录表单初始化

--- app/main/account.py
+++ app/main/account.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, current_app, render_template
 from app.forms.account import LoginForm
+from flask import Blueprint, current_app, redirect, render_template, request, url_for
 
 bp_login = Blueprint("bp_login", __name__, url_prefix="/account")
 
 
-@bp_login.route("/login")
+@bp_login.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('bp_index.index'))
     return render_template("account/login.html", form=form)
